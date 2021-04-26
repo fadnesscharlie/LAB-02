8939b2033eec3edfb8b73b0c691c436337242d42
Charlie Fadness <boy_916@hotmail.com>
2021-04-25 22:47:04 -0700

    updating with use strict

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,8 +1,9 @@
-
+"use strict";
 //####### 5 questions about me  ###########
 
 // First Quiz will be if I like Cats
 var quizOne = function() {
+    let catLower;
     let response = prompt("Do you think I like Cats? Please enter Yes or No")
     catLower = response.toLowerCase()
     // console.log(catLower + "1")
@@ -23,15 +24,16 @@ var quizOne = function() {
 
 // Second Quix will be if I like to bake
 var quizTwo = function() {
+    let bake;
     let response = prompt("Do you think I like to Bake? Please enter Yes or No")
     bake = response.toLowerCase()
-    console.log(bake + "1")
+    // console.log(bake + "1")
     while (bake != 'no' && bake != 'yes' && bake != 'y' && bake != 'n') {
         bake = prompt('I\'m sorry, please enter a Yes or No.')
         bake = bake.toLowerCase()
-        console.log(bake + "2")
+        // console.log(bake + "2")
     }
-    console.log(bake + "3")
+    // console.log(bake + "3")
     if (bake == "yes" || bake == 'y') {
         alert("That's amazing! I love Pastries!")
     } else if (bake == "no" || bake == 'n') {
@@ -43,15 +45,16 @@ var quizTwo = function() {
 
 // Third Quiz will be if I live in bothell
 var quizThree = function() {
+    let live;
     let response = prompt("Do I live in Bothell, WA? Please enter Yes or No")
     live = response.toLowerCase()
-    console.log(live + "1")
+    // console.log(live + "1")
     while (live != 'no' && live != 'yes' && live != 'y' && live != 'n') {
         live = prompt('I\'m sorry, please enter a Yes or No.')
         live = live.toLowerCase()
-        console.log(live + "2")
+        // console.log(live + "2")
     }
-    console.log(live + "3")
+    // console.log(live + "3")
     if (live == "yes" || live == 'y') {
         alert("Your correct! I actually live in Renton!")
     } else if (live == "no" || live == 'n') {
@@ -63,15 +66,16 @@ var quizThree = function() {
 
 // Fourth Quiz will be if I like outdoors
 var quizFour = function() {
+    let outDoor;
     let response = prompt("Do you think I like the outdoors? Please enter Yes or No")
     outDoor = response.toLowerCase()
-    console.log(outDoor + "1")
+    // console.log(outDoor + "1")
     while (outDoor != 'no' && outDoor != 'yes' && outDoor != 'y' && outDoor != 'n') {
         outDoor = prompt('I\'m sorry, please enter a Yes or No.')
         outDoor = outDoor.toLowerCase()
-        console.log(outDoor + "2")
+        // console.log(outDoor + "2")
     }
-    console.log(outDoor + "3")
+    // console.log(outDoor + "3")
     if (outDoor == "yes" || outDoor == 'y') {
         alert("Your correct! I actually like the outdoors!")
     } else if (outDoor == "no" || outDoor == 'n') {
@@ -83,15 +87,16 @@ var quizFour = function() {
 
 // Fifth Quiz will be if I have 10 Cats
 var quizFive = function() {
+    let catMany;
     let response = prompt("Do you think I have over 10 cats? Please enter Yes or No")
     catMany = response.toLowerCase()
-    console.log(catMany + "1")
+    // console.log(catMany + "1")
     while (catMany != 'no' && catMany != 'yes' && catMany != 'y' && catMany != 'n') {
         catMany = prompt('I\'m sorry, please enter a Yes or No.')
         catMany = catMany.toLowerCase()
-        console.log(catMany + "2")
+        // console.log(catMany + "2")
     }
-    console.log(catMany + "3")
+    // console.log(catMany + "3")
     if (catMany == "yes" || catMany == 'y') {
         alert("Your correct! I do not have 10! I just have 2!")
     } else if (catMany == "no" || catMany == 'n') {
